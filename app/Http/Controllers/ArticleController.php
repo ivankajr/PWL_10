8cf50958788f00369525c8a9187fbf0816f8ec49
ivankajr <ivnka24@gmail.com>
2021-04-14 08:43:43 +0700

    add controller update and edit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use PDF;
 
 class ArticleController extends Controller
 {
@@ -67,7 +68,9 @@ class ArticleController extends Controller
      */
     public function edit(Article $article)
     {
-        //
+        $article = Article::find($id);
+        
+        return view('articles.edit', ['article' => $article]);
     }
 
     /**
@@ -79,7 +82,19 @@ class ArticleController extends Controller
      */
     public function update(Request $request, Article $article)
     {
-        //
+        $article = Article::find($id);
+
+        $article->title = $request->title;
+        $article->content = $request->content;
+
+        if($article->featured_image && file_exists('app/public/' . $article->featured_image)) {
+            \Storage::delete('public/' . $article->featured_image);
+        }
+        $image_name = $request->file('image')->store('images', 'public');
+        $article->featured_image = $image_name;
+
+        $article->save();
+        return 'Artikel berhasil dirubah';
     }
 
     /**
